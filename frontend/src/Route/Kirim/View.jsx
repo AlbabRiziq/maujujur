814da67e97dadd91d1f12d2c6fb37f2edae43874
AlbabRiziq <albabriziq123@gmail.com>
2023-12-24 19:30:12 +0700

    updates

--- frontend/src/Route/Kirim/View.jsx
+++ frontend/src/Route/Kirim/View.jsx
@@ -9,17 +9,17 @@ function View() {
       <br />
       <br />
       <div className="flex items-center flex-col ">
-        <div className="text-sm p-5 mx-2 text-white rounded-xl bg-[#427D9D] text-center">
+        {/* <div className="text-sm p-5 mx-2 text-white rounded-xl bg-[#427D9D] text-center">
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta a
             vero consequuntur culpa in fugiat iusto beatae illum. Quaerat
             distinctio voluptas vitae perferendis doloribus autem impedit
             aliquam totam facilis? Officia!
           </p>
-        </div>
+        </div> */}
 
         {/* Tampilan komentar */}
-
+        {/* 
         <div className="flex w-screen p-5">
           <div className="bg-[#427d9d] text-white p-2 px-3 rounded-lg">
             <h1 className="text-sm mb-2 font-bold">ANONYMOUS</h1>
@@ -28,7 +28,7 @@ function View() {
               Voluptates, recusandae.
             </p>
           </div>
-        </div>
+        </div> */}
 
         <form className="flex flex-col items-center">
           <textarea
